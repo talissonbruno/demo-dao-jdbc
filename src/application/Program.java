12ad3661d5f0ca2d9071f0367aa0065fcc3d5797
Talisson Bruno <talissonbrsouza@gmail.com>
2019-05-29 23:57:26 -0300

    Correcao do arquivo DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,36 +5,34 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
 
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 5);
 
 			int rowsAffected = st.executeUpdate();
 
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		} 
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
-
 }
